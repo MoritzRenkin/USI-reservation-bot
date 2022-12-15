41338fc24c87f446eec9183368e8874a2c063f05
Moritz Renkin <moritz@renkin.cc>
2022-12-15 20:11:17 +0100

    added test skeleton

---
+++ tests/test_login.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class LoginTest(unittest.TestCase):
+    def test_login_tu(self):
+        pass # TODO
+        #self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
